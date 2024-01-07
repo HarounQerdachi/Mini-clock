3e7c203cf5850fc2cd5fc9ab7bf7d836d3af3bac
HarounQerdachi <haroun.qerdachi@gmail.com>
2024-01-07 22:36:20 +0100

    Some improvements have been done.

--- src/app.py
+++ src/app.py
@@ -1,8 +1,31 @@
 import flet as ft
 
 def main(page: ft.Page):
-    t = ft.Text(value="Hello, world!", color="green")
-    page.controls.append(t)
+    # Set a title and adding some color to the background
+    page.title= "Mini clock"
+    page.bgcolor= "#323232"
+
+    # Set the width and the height of the app
+    page.window_width= 1000
+    page.window_height= 550
+    # page.theme_mode= "dark"
+
+    # Disable resizing the app
+    page.window_resizable= False
+
+    #Adding fonts
+    page.fonts= {
+            "Open Sans": "fonts/OpenSans-Regular.ttf",
+            "Fanlste": "fonts/fanlste.otf",
+        }
+
+    title= ft.Text(value= "Mini Clock", size= 28, weight= "w700", color= "white", font_family= 'Open Sans', text_align="center")
+    description= ft.Text(value= "The pomodoro timer that empowers you to take control of your time and optimize your work sessions.", size= 14, color= "white", font_family= 'Open Sans',)
+
+    page.add(
+        title, description
+    )
+
     page.update()
 
-ft.app(target=main)
+ft.app(target=main, assets_dir="../assets")
